b8118e42b227c8b7d05dc31363ad56181054d5f7
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-09 12:03:30 +0900

    finish the chapter 02.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 5;
+
+	void Update () 
+	{
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		float y = Input.GetAxisRaw ("Vertical");
+
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		rigidbody2D.velocity = direction * speed;
+	}
+}
